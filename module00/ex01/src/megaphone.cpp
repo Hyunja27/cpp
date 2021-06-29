ad4f6e17ad228c2ce3eb3b2844b7a3837793997e
Hyunja27 <123gk@naver.com>
2021-06-29 15:36:17 +0900

    06/29_18:42_init_cpp

---
+++ module00/ex01/src/megaphone.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+
+int main(int ac, char *av[])
+{
+    int i = 0;
+    unsigned long j = -1;
+
+    std::string str;
+    if (ac == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+        std::cout << std::endl;
+        return (-1);
+    }
+    while (++i < ac)
+    {
+        str = av[i];
+        while(++j < str.size())
+        {
+            if (str[j] == 32)
+                std::cout << str[j];
+            else
+            {
+                str[j] = std::toupper(str[j]);
+                std::cout << str[j];
+            }
+        }
+        j = -1;
+    }
+    std::cout << std::endl;
+    return (0);
+}

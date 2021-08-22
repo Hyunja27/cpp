66e4ba5501eb53c42db79944a13ac1f730cf6ffd
Hyunja27 <123gk@naver.com>
2021-08-22 12:34:53 +0900

    08/22_module08_eval_ready

--- module08/ex01/src/main.cpp
+++ module08/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hyunja <hyunja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/04 16:08:38 by spark             #+#    #+#             */
-/*   Updated: 2021/08/11 17:12:34 by hyunja           ###   ########.fr       */
+/*   Updated: 2021/08/22 12:16:11 by hyunja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int main(void)
     sp.addNumber(17);
     sp.addNumber(9);
     sp.addNumber(11);
+    // sp.addNumber(214748364);
     
     std::cout << sp.shortestSpan() << std::endl;
     std::cout << sp.longestSpan() << std::endl << std::endl;
@@ -29,11 +30,17 @@ int main(void)
 
     std::cout << " ==== New addnumber test == == " << std::endl << std::endl;
 
-    Span tmp = Span(10000);
+    Span tmp(10000);
 
     std::vector<int> v(10000);
-    
     v[1989] = 909;
+    v[9172] = 100;
+
+    // std::vector<int> v;
+    // for (int i = 0; i < 10000; i++) 
+    // {
+	// 	v.push_back(i + 1);
+	// }
 
     tmp.addNumber(v.begin(), v.end());
     
